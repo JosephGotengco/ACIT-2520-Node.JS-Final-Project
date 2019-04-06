f576e66a8bd3152c4409f6a2f2835c1c0bde6ab5
taylor-artemenko <48867121+taylor-artemenko@users.noreply.github.com>
2019-04-06 14:19:06 -0700

    Add files via upload
    web app with login and registration (newest one)

--- Web Page/webapp.js
+++ Web Page/webapp.js
@@ -1,6 +1,8 @@
 const express = require('express');
 // const axios = require('axios');
 const hbs = require('hbs');
+const utils = require('./utils.js');
+const bodyParser = require('body-parser');
 
 var app = express();
 
@@ -12,7 +14,10 @@ app.set('view engine', 'hbs');
 
 // Web Pages
 app.use(express.static(__dirname));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/', (request, response) => {
     response.render('index.hbs', {
@@ -25,7 +30,7 @@ app.get('/code', (request, response) => {
     response.render('code.hbs', {
         title: 'Code Page',
         header: "This is about me!"
-    })
+    });
 });
 
 app.get('/projects', (request, response) => {
@@ -35,5 +40,42 @@ app.get('/projects', (request, response) => {
     });
 });
 
-app.listen(8080);
-console.log("Webapp is now operational!")
+app.listen(8080, () => {
+    console.log('Server is up and running');
+    utils.init()
+});
+
+app.post('/login', (request, response) => {
+    var db = utils.getDb();
+    db.collection('users').find({}).toArray((err, result) => {
+        if (err) {
+            response.send('Unable to get login right now');
+        }
+        for (i=0; i < result.length; i++) {
+            if (request.body.username === result[i].username) {
+                if (request.body.password === result[i].password) {
+                    console.log('hey');
+                    response.render('index.hbs', {
+                        success_login: 'You are logged in!'
+                    })
+                } else {
+                    response.render('index.hbs', {
+                        success_login: 'Invalid login info'
+                    })
+                }
+            } else {
+                response.render('index.hbs', {
+                    success_login: 'Invalid login info'
+                })
+            }
+        }
+    });
+});
+
+app.post('/register', function (req, res) {
+    var db = utils.getDb();
+    db.collection('users').insertOne(req.body);
+    res.render('index.hbs', {
+        success_register: 'Thank You for Registering!'
+    })
+});

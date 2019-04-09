c9d098809ffc0e00577925b85b292aa943fd62d0
donkey nong <48866991+derekng01@users.noreply.github.com>
2019-04-08 20:37:18 -0700

    Function 3 + stylings
    We're almost there!

--- Web Page/webapp.js
+++ Web Page/webapp.js
@@ -9,7 +9,7 @@ var app = express();
 // Cookie Code
 // Ignore this line underneath I just copied it from a website LOL
 app.use(session({secret: 'XASDASDA'}));
-var ssn;
+var ssn ;
 // Cookie Code
 
 
@@ -27,9 +27,9 @@ app.set('view engine', 'hbs');
 // Web Pages
 app.use(express.static(__dirname));
 
-
+// --------------- index page  --------------- //
 app.get('/', (request, response) => {
-    ssn = request.session;
+    ssn=request.session;
     response.render('index.hbs', {
         title: "Home Page",
         header: "Welcome to Home!"
@@ -37,60 +37,52 @@ app.get('/', (request, response) => {
     ssn.comport;
     ssn.command;
 });
-//
-// app.post('/register', function (request, response) {
-//     var db = utils.getDb();
-//     db.collection('users').insertOne(request.body);
-//     response.render('index.hbs', {
-//         success_register: 'Thank You for Registering!'
-//     });
-// });
-
 app.post('/register', function (request, response) {
     var db = utils.getDb();
-    request.body["data"] = "";
+    request.body["data"] = ""
+    db.collection('users').insertOne(request.body);
+    response.render('index.hbs', {
+        success_register: 'Thank You for Registering!'
+    });
+});
+app.post('/login', (request, response) => {
+    ssn = request.session;
+    var db = utils.getDb();
     db.collection('users').find(request.body).toArray((err, result) => {
         if (result.length === 0) {
-            db.collection('users').insertOne(request.body);
             response.render('index.hbs', {
-                success_register: 'Thank You for Registering!'
+                success_login: 'Invalid Login Info!'
             })
-        }else{
+        } else {
             response.render('index.hbs', {
-                success_register: 'Account Already Exists'
+                success_login: 'You Are Now Logged In!'
             })
+        
+            ssn.username=request.body.username;
+            ssn.password=request.body.password;
+            
         }
     });
 });
+// --------------- index page  --------------- //
 
 
+
+// --------------- code page  --------------- //
 app.get('/code', (request, response) => {
-    response.render('code.hbs', {
-        title: 'Code Page',
-        header: "This is about me!",
-        username: ssn.username
+    var db = utils.getDb();
+    db.collection('users').find({username: ssn.username}).toArray((err, items) => {
+        console.log(items);
+        data = items[0]["data"]
+        response.render('code.hbs', {
+            title: 'Code Page',
+            header: "This is about me!",
+            username: ssn.username,
+            data: data
+        });
     });
 });
 
-// app.post('/code-save', (request, response) => {
-//     var db = utils.getDb();
-//     username = ssn.username
-//     console.log(username);
-//     data = request.body.data
-//     console.log(data);
-//
-//      // db.collection('users').insertOne({username: username, data: data});
-//     //db.collection.update(find(username), data: data)
-//     // db.collection('users').find(request.username).toArray((err, result) => {
-//     //
-//     //
-//     //     }
-//
-//     // request.body["data"] = data;
-//     response.render('code.hbs', {
-//         success: 'File Has Been Saved!'
-//     })
-// })
 app.post('/code-save', (request, response) => {
     var db = utils.getDb();
 
@@ -103,6 +95,7 @@ app.post('/code-save', (request, response) => {
     db.collection('users').findOneAndUpdate({username: username}, {'$set': {'data': data}}, (err, item) => {
         console.log(item)
     });
+
     response.render('code.hbs', {
         success: 'File Has Been Saved!',
         title: 'Code Page',
@@ -111,16 +104,8 @@ app.post('/code-save', (request, response) => {
     })
 })
 
-app.get('/code-get', function (request, response) {
-    var db = utils.getDb();
-    username = request.body.username;
-    console.log(username);
+// --------------- code page  --------------- //
 
-    db.collection('users').find({username: username}).toArray((err, items) => {
-        response.send(items);
-    });
-
-});
 
 app.listen(8080, () => {
     console.log('Server is up and running');
@@ -128,19 +113,5 @@ app.listen(8080, () => {
 });
 
 
-app.post('/login', (request, response) => {
-    ssn = request.session;
-    var db = utils.getDb();
-    db.collection('users').find(request.body).toArray((err, result) => {
-        if (result.length === 0) {
-            response.render('index.hbs', {
-                success_login: 'Invalid Login Info!'
-            })
-        } else {
-            response.render('index.hbs', {
-                success_login: 'Login succesful!'
 
-            })
-            }
-        })
-});
+
